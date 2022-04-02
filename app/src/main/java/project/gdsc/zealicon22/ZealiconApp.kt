5a3522f26435936ded63f21a7c97333f28723edd
dheerajkotwani <dheeraj.kotwani@zomato.com>
2022-04-03 01:12:18 +0530

    ⚙️ Setup project

---
+++ app/src/main/java/project/gdsc/zealicon22/ZealiconApp.kt
@@ -0,0 +1,22 @@
+package project.gdsc.zealicon22
+
+import android.app.Application
+import androidx.viewbinding.BuildConfig
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+/**
+ * @author Dheeraj Kotwani on 02/04/22.
+ */
+@HiltAndroidApp
+class ZealiconApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+}
