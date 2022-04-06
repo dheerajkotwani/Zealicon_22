a573ef129e74fce1749e3ca07f3047c2e822e7ac
dhruvbaliyan <dhruvbaliyan1001@gmail.com>
2022-04-06 10:11:20 +0530

    Fix and clean up EventAdapter

--- app/src/main/java/project/gdsc/zealicon22/adapter/EventsAdapter.kt
+++ app/src/main/java/project/gdsc/zealicon22/adapter/EventsAdapter.kt
@@ -5,10 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.RecyclerView
 import project.gdsc.zealicon22.R
 import project.gdsc.zealicon22.databinding.ItemEventCardBinding
 import project.gdsc.zealicon22.interfaces.ConstraintInstructions
+import project.gdsc.zealicon22.utils.animateOnClick
 import project.gdsc.zealicon22.utils.updateConstraints
 
 
@@ -16,6 +18,8 @@ class EventsAdapter(
     private val events: ArrayList<String>,
     private val context: Context
 ): RecyclerView.Adapter<EventsAdapter.EventViewHolder>() {
+
+    private val ROTATION_CONST = 2f
 
     class EventViewHolder(item: View): RecyclerView.ViewHolder(item){
         val binding = ItemEventCardBinding.bind(item)
@@ -40,12 +44,11 @@ class EventsAdapter(
         with(holder){
 
             binding.eventName.text = events[position]
-            if (position % 2 == 0){ // even item case
+            if (position % 2 == 0){ // even item case (shifts info towards right)
 
                 // This code disconnects and connects constraints
                 // and sends event info Text Views to right side in case of even position element
                 // help from here -> [https://medium.com/geekculture/programatically-update-view-constraints-in-constraint-layout-f4eb1b6dfc38]
-
                 val constraintInstructions: List<ConstraintInstructions> = listOf(
                     ConstraintInstructions.DisconnectConstraint(binding.eventName.id, ConstraintSet.START),
                     ConstraintInstructions.ConnectConstraint(binding.eventName.id, ConstraintSet.END, 0, ConstraintSet.END),
@@ -68,12 +71,36 @@ class EventsAdapter(
                     rotationX = 180f
                     rotationY = 180f
                 }
+            }
 
-
-            } else { // odd item case
-
+            when (position % 3) {
+                0 -> {
+                    binding.eventConstraint.rotation = 0f
+                    setEventBackgroundImage(binding, R.drawable.events_background1)
+                }
+                1 -> {
+                    binding.eventConstraint.rotation = -ROTATION_CONST
+                    setEventBackgroundImage(binding, R.drawable.events_background2)
+                }
+                2 -> {
+                    binding.eventConstraint.rotation = ROTATION_CONST
+                    setEventBackgroundImage(binding, R.drawable.events_background3)
+                }
+            }
+            binding.eventConstraint.setOnClickListener {
+                animateOnClick(it)
             }
         }
+    }
+
+    private fun setEventBackgroundImage(binding: ItemEventCardBinding, drawableId: Int) {
+        binding.eventBackgroundImage.setImageDrawable(
+            ResourcesCompat.getDrawable(
+                context.resources,
+                drawableId,
+                null
+            )
+        )
     }
 
     override fun getItemCount() = events.size
